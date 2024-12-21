48b1fec59820e62f00ebc3218a957a226a4e90df
codewithshorty <codewithshorty@gmail.com>
2024-12-21 15:11:44 +0100

    Inititial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ChatBotStart from "./components/ChatBotStart";
+
+export default function App() {
+  return (
+    <div>
+      <ChatBotStart />
+    </div>
+  );
+}
---
+++ src/components/ChatBotStart.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ChatBotStart() {
+  return <div>ChatBotStart</div>;
+}
