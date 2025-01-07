4e77b0d9fc85e51340f16978ed3ed51d2cdec087
codewithshorty <codewithshorty@gmail.com>
2025-01-07 14:27:51 +0100

    Initial chats state created in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,22 @@ import { useState } from "react";
 export default function App() {
   const [isChatting, setIsChatting] = useState(false);
 
+  //Creating the state for storing the chats
+  const [chats, setChats] = useState([]);
+
   const handleChatStart = () => {
     setIsChatting(true);
+    // Checking and creating of initial chat object
+    if (chats.length === 0) {
+      const newChat = {
+        id: `Chat log: ${new Date().toLocaleDateString(
+          "en-GB"
+        )} ${new Date().toLocaleTimeString()}`,
+        messages: [],
+      };
+
+      setChats([newChat]);
+    }
   };
 
   const handleGoBack = () => {
@@ -16,7 +30,7 @@ export default function App() {
   return (
     <div className="container">
       {isChatting ? (
-        <ChatBotApp onGoBack={handleGoBack} />
+        <ChatBotApp onGoBack={handleGoBack} chats={chats} setChats={setChats} />
       ) : (
         <ChatBotStart onChatStart={handleChatStart} />
       )}
